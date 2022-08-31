45248a341d6bf1f73249b346698ea07a60c084ac
Geonsu Lee <legonsu2@gmail.com>
2022-08-31 23:15:08 +0900

    update

--- tripPrjTest/src/main/java/com/controller/PlaceReviewController.java
+++ tripPrjTest/src/main/java/com/controller/PlaceReviewController.java
@@ -1,11 +1,22 @@
 package com.controller;
 
+import java.io.File;
+import java.util.List;
+import java.util.StringTokenizer;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
+import com.model.MemberBean;
+import com.model.PlaceReviewBean;
 import com.service.MemberService;
 import com.service.PlaceReplyService;
 import com.service.PlaceReviewService;
@@ -14,7 +25,7 @@ import com.service.PlaceReviewService;
 public class PlaceReviewController {
 
 	@Autowired
-	private PlaceReviewService serviec;
+	private PlaceReviewService service;
 	
 	@Autowired
 	private PlaceReplyService placeReplyService;
@@ -38,5 +49,229 @@ public class PlaceReviewController {
 	}
 	
 	
+	// 리뷰 작성
+	@RequestMapping("placeReviewWrite")
+	public String reviewWrite(@RequestParam(value ="starpoint") double rev_rate, 
+			@RequestParam("place_id") int place_id, @RequestParam("page") int page,
+			MultipartHttpServletRequest mtf, Model model, HttpServletRequest request,
+			PlaceReviewBean placereview, HttpSession session) throws Exception {
+
+		//double rev_rate = Double.parseDouble("starValue");
+		String id = (String) session.getAttribute("id");
+		
+		System.out.println("controller까지 들어옴");
+		System.out.println("rev_rate="+rev_rate);
+		System.out.println("page="+page);
+		System.out.println("place_id="+place_id);
+		System.out.println("id="+id);
+		
+		List<MultipartFile> fileList = mtf.getFiles("rev_photos");
+
+		String path = request.getRealPath("upload");
+		System.out.println("path=" + path);
+
+		StringBuilder files = new StringBuilder();
+		
+		int size = 0;
+		
+		for (MultipartFile mf : fileList) {
+
+			int result=0;
+			size = (int) mf.getSize();
+			
+			if(size > 0) {
+			
+			String originFileName = mf.getOriginalFilename();
+			
+			String file[] = new String[2];
+			StringTokenizer st = new StringTokenizer(originFileName, ".");
+			file[0] = st.nextToken();		// 파일명		Koala
+			file[1] = st.nextToken();		// 확장자	    jpg
+			
+			if(size > 10000000){				// 100KB
+				result=1;
+				model.addAttribute("result", result);
+				
+				return "place_review/place_review_upload_result";
+				
+			}else if(!file[1].equals("jpg") &&
+					 !file[1].equals("gif") &&
+					 !file[1].equals("png") ){
+				
+				result=2;
+				model.addAttribute("result", result);
+				
+				return "place_review/place_review_upload_result";
+			}
+			
+			System.out.println("originFileName=" + originFileName);
+
+			String safeFile = System.currentTimeMillis() + originFileName;
+
+			files.append(safeFile + ",");
+			System.out.println("safeFile" + safeFile);
+
+			mf.transferTo(new File(path+"/"+safeFile));
+			
+			}
+			
+		}
+		
+		if(size > 0) {
+			placereview.setRev_photo(files.toString());
+			
+		}else {
+			placereview.setRev_photo(null);
+
+		}
+			
+		System.out.println("첨부파일들:"+files.toString());
+			
+		
+		// 회원 상세정보 구해오기
+		MemberBean member = memberService.userCheck(id);
+		
+		// 회원 프로필 추출
+		String userphoto = member.getProfile();
+		System.out.println("userphoto="+userphoto);
+
+		// 회원 프로필 리뷰에 저장
+		placereview.setUserphoto(userphoto);
+		
+		
+		placereview.setId(id);
+		placereview.setRev_rate(rev_rate);
+		service.insertReview(placereview);
+		
+		
+		double starAvg = service.getPlaceStar(place_id);
+
+		System.out.println("starAvg="+starAvg);
+		
+		System.out.println("insert 완료");
+
+		return "redirect:/placeDetail?place_id=" + place_id + "&page=" + page + "&state=cont";
+
+	}
+	
+	
+	// 리뷰 상세정보를 구해서 수정모달 페이지로 이동
+		@RequestMapping(value ="/placeReviewModifyModal")
+		public String modal(@RequestParam("place_rev_id") int place_rev_id, @RequestParam("place_id") int place_id, 
+							@RequestParam("page") int page, Model model) throws Exception {
+			
+			System.out.println("place 수정 modal controller 진입");
+			
+			PlaceReviewBean placeReviewDetail = service.reviewDetail(place_rev_id);
+			
+			model.addAttribute("placeReviewDetail", placeReviewDetail);
+			model.addAttribute("place_rev_id", place_rev_id);
+			model.addAttribute("place_id", place_id);
+			model.addAttribute("page", page);
+			
+			return "place_review/place_review_modify_modal";
+		}
+		
+		
+
+		// 리뷰 수정
+		@RequestMapping("placeReviewModify")
+		public String reviewModifyForm(@RequestParam("place_rev_id") int place_rev_id, @RequestParam("place_id") int place_id,
+				@RequestParam("starpoint") double rev_rate, @RequestParam("page") int page,
+				MultipartHttpServletRequest mtf, Model model, HttpServletRequest request,
+				PlaceReviewBean placereview, HttpSession session) throws Exception{
+			
+			System.out.println("review modify controller진입");
+			System.out.println("rev_rate="+rev_rate);
+			System.out.println("page="+page);
+			System.out.println("place_id="+place_id);
+			
+			
+			PlaceReviewBean oldreview = service.reviewDetail(place_rev_id);
+
+			
+			
+			System.out.println("rev_rate="+rev_rate);
+			if(Math.abs(rev_rate) > 0) {
+				placereview.setRev_rate(rev_rate);
+			}else {
+				placereview.setRev_rate(oldreview.getRev_rate());
+			}
+			
+			
+			List<MultipartFile> fileList = mtf.getFiles("rev_photos1");
+			String path = request.getRealPath("upload");
+			System.out.println("place_rev_id="+place_rev_id);
+			
+			placereview.setPlace_rev_id(place_rev_id);
+			
+			
+				StringBuilder files = new StringBuilder();
+				int size = 0;			
+				
+				for (MultipartFile mf : fileList) {
+					int result=0;
+					
+					String originFileName = mf.getOriginalFilename();
+					size = (int) mf.getSize();
+					
+					
+					if(size > 0) {
+						
+					String file[] = new String[2];
+					StringTokenizer st = new StringTokenizer(originFileName, ".");
+					file[0] = st.nextToken();		// 파일명		Koala
+					file[1] = st.nextToken();		// 확장자	    jpg
+					
+					if(size > 10000000){				// 100KB
+					
+						result=1;
+						model.addAttribute("result", result);
+						
+						return "place_review/review_upload_result";
+						
+					}else if(!file[1].equals("jpg") &&
+							 !file[1].equals("gif") &&
+							 !file[1].equals("png") ){
+						
+						result=2;
+						model.addAttribute("result", result);
+						
+						return "place_review/review_upload_result";
+					}
+					
+					}
+					
+					
+					System.out.println("originFileName=" + originFileName);
+
+					String safeFile = System.currentTimeMillis() + originFileName;
+
+					files.append(safeFile + ",");
+					System.out.println("safeFile" + safeFile);
+
+					mf.transferTo(new File(path+"/"+safeFile));
+				} // for end
+				
+				
+				if(size > 0) {
+					placereview.setRev_photo(files.toString());
+				}else {
+					placereview.setRev_photo(oldreview.getRev_photo());
+				}
+
+		
+			service.reviewModify(placereview);
+		
+			
+			place_id = placereview.getPlace_id();
+			double starAvg = service.getPlaceStar(place_id);
+
+			System.out.println("starAvg="+starAvg);
+			
+			
+			return "redirect:/placeDetail?place_id=" + place_id + "&page=" + page + "&state=cont";
+		}
+		
 	
 }
